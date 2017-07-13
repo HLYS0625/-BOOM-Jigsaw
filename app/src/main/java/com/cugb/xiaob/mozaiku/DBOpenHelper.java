bfba98822f99deb1f8026f171c03f24718784cf2
MineRiko <326599466@qq.com>
2017-07-13 16:54:18 +0800

    增加了自定义用户头像并存储的功能。
    另外由于更改了数据库结构（加了一列head），将『my.db』数据库version升级为2
    由于升级过程中使用了删除重建的方法，旧的数据库数据会丢失。

--- app/src/main/java/com/cugb/xiaob/mozaiku/DBOpenHelper.java
+++ app/src/main/java/com/cugb/xiaob/mozaiku/DBOpenHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.io.File;
+
 /**
  * Created by xiaob on 2017/7/12.
  */
@@ -11,20 +13,41 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBOpenHelper extends SQLiteOpenHelper {
 
     //构造函数，创建一个新的数据库
-    public DBOpenHelper(Context context,int version) {
+    public DBOpenHelper(Context context, int version) {
         super(context, "my.db", null, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE scoreInfo(recordNo INTEGER PRIMARY KEY AUTOINCREMENT,userName VARCHAR(20) NOT NULL," +
-                "difficult INTEGER NOT NULL,record INTEGER NOT NULL)");
+                "difficult INTEGER NOT NULL,record INTEGER NOT NULL);");
         db.execSQL("CREATE TABLE userInfo(userName VARCHAR(20) PRIMARY KEY ,password VARCHAR(20) NOT NULL," +
-                "easyHS INTEGER DEFAULT 0,normalHS INTEGER DEFAULT 0,hardHS INTEGER DEFAULT 0)");
-
+                "head INTEGER DEFAULT 0,easyHS INTEGER DEFAULT 0,normalHS INTEGER DEFAULT 0,hardHS INTEGER DEFAULT 0);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        rebuild(db);
+    }
+
+    public void rebuild(SQLiteDatabase db) {
+        db.execSQL("DROP TABLE scoreInfo;");
+        db.execSQL("DROP TABLE userInfo");
+        db.execSQL("CREATE TABLE scoreInfo(recordNo INTEGER PRIMARY KEY AUTOINCREMENT,userName VARCHAR(20) NOT NULL," +
+                "difficult INTEGER NOT NULL,record INTEGER NOT NULL);");
+        db.execSQL("CREATE TABLE userInfo(userName VARCHAR(20) PRIMARY KEY ,password VARCHAR(20) NOT NULL," +
+                "head INTEGER DEFAULT 0,easyHS INTEGER DEFAULT 0,normalHS INTEGER DEFAULT 0,hardHS INTEGER DEFAULT 0);");
+    }
+
+    public void cleanScore(SQLiteDatabase db) {
+        db.execSQL("DROP TABLE scoreInfo;");
+        db.execSQL("CREATE TABLE scoreInfo(recordNo INTEGER PRIMARY KEY AUTOINCREMENT,userName VARCHAR(20) NOT NULL," +
+                "difficult INTEGER NOT NULL,record INTEGER NOT NULL);");
+    }
+
+    public void cleanUser(SQLiteDatabase db) {
+        db.execSQL("DROP TABLE userInfo");
+        db.execSQL("CREATE TABLE userInfo(userName VARCHAR(20) PRIMARY KEY ,password VARCHAR(20) NOT NULL," +
+                "head INTEGER DEFAULT 0,easyHS INTEGER DEFAULT 0,normalHS INTEGER DEFAULT 0,hardHS INTEGER DEFAULT 0);");
     }
 }
